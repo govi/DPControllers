8792a06ff75e156c8851b2c3216979760a6f3e24
Govi <govi@email.com>
2013-07-31 17:30:58 +0100

    adding a way to get the current progress color

--- Slidey/DPNiftyProgressBar.m
+++ Slidey/DPNiftyProgressBar.m
@@ -120,6 +120,10 @@
     [self setProgress:self.progress];
 }
 
+-(UIColor *)color {
+    return progressView.backgroundColor;
+}
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 /*- (void)drawRect:(CGRect)rect
